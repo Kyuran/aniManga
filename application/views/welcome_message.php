81b8b31d77b9c9b1e6caaaba1716d6e3af745ad2
Kyuran <bey.sofiene@gmail.com>
2014-10-14 11:38:24 +0200

    add directory

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -70,7 +70,7 @@
 	<h1>Welcome to CodeIgniter!</h1>
 
 	<div id="body">
-        Hello boy
+        Hello boyr
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
